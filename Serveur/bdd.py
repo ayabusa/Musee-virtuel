9bd51fa75806b2b1aae93fc925da253c2d8c67f4
BuildTools <unconfigured@null.spigotmc.org>
2024-11-29 15:11:46 +0100

    creation de placeholder pour bdd.py

---
+++ Serveur/bdd.py
@@ -0,0 +1,34 @@
+def recuperer_liste_couloir()->list:
+    # Renvoie une liste des id de tout les couloirs
+    # A faire
+    return []
+
+def recuperer_theme_couloir(couloir_id: int)->str:
+    # Renvoie le thème du couloir à partir de son id
+    # A faire
+    pass
+
+def recuperer_liste_tableau(couloir_id: int)->list:
+    # Renvoie une liste des id des tableaux appartenant a un couloir
+    # A faire
+    pass
+
+def recuperer_nom_tableau(tableau_id: int)->str:
+    # Renvoie le nom du tableau à partir de son id
+    # A faire
+    pass
+
+def recuperer_auteur_tableau(tableau_id: int)->str:
+    # Renvoie l'auteur du tableau à partir de son id
+    # A faire
+    pass
+
+def recuperer_description_tableau(tableau_id: int)->str:
+    # Renvoie la description du tableau à partir de son id
+    # A faire
+    pass
+
+def recuperer_date_tableau(tableau_id: int)->str:
+    # Renvoie la date du tableau à partir de son id
+    # A faire
+    pass
