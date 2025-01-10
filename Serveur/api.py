bd5d893f41da7da9a8139f2551393bb0e91f2d8d
BuildTools <unconfigured@null.spigotmc.org>
2025-01-10 15:33:59 +0100

    implémentation finit de l'API

--- Serveur/api.py
+++ Serveur/api.py
@@ -23,19 +23,21 @@ GET /get_tableaux_from_couloir/&ltcouloir_id&gt</br>
 
 @app.route('/get_couloirs_liste')
 def get_couloirs_liste():
-    liste_des_couloirs = {1: "de",2: "dqzd",3: "dqzdq"}
-    for i in bdd.recuperer_liste_couloir():
-        liste_des_couloirs[i] = bdd.recuperer_theme_couloir(i)
+    db = bdd.DB()
+    liste_des_couloirs= {}
+    for i in db.recuperer_liste_couloir():
+        liste_des_couloirs[i] = db.recuperer_theme_couloir(i)
     return jsonify(liste_des_couloirs)
 
 @app.route('/get_tableaux_from_couloir/<couloir_id>')
 def get_tableaux_from_couloir(couloir_id):
+    db = bdd.DB()
     liste_des_tableaux = {1: ["nom", "auteur_id", "tags", "descriptionn", "date"], 2: ["nom", "auteur_id", "tags", "descriptionn", "date"]}
-    for idt in bdd.recuperer_liste_tableau(couloir_id):
-        liste_des_tableaux[idt] = [bdd.recuperer_nom_tableau(idt),
-                                   bdd.recuperer_auteur_tableau(idt),
-                                   bdd.recuperer_description_tableau(idt),
-                                   bdd.recuperer_date_tableau(idt)]
+    for idt in db.recuperer_liste_tableau(couloir_id):
+        liste_des_tableaux[idt] = [db.recuperer_nom_tableau(idt),
+                                   db.recuperer_auteur_tableau(idt),
+                                   db.recuperer_description_tableau(idt),
+                                   db.recuperer_date_tableau(idt)]
     return jsonify(liste_des_tableaux)
 
 
--- Serveur/bdd.py
+++ Serveur/bdd.py
@@ -1,47 +1,45 @@
 import sqlite3
 
-with open('bdd.sql', 'r') as sql_file:
-    sql_script = sql_file.read()
+class DB:
+    def __init__(self):
+        with open('bdd.sql', 'r') as sql_file:
+            sql_script = sql_file.read()
 
-con = sqlite3.connect("../bdd.db")
-cur = con.cursor()
-cur.executescript(sql_script)
+        self.con = sqlite3.connect("bdd.db")
+        self.cur = self.con.cursor()
+        self.cur.executescript(sql_script)
+        self.con.commit()
+    def recuperer_liste_couloir(self)->list:
+        # Renvoie une liste des id de tout les couloirs
+        res = self.cur.execute("SELECT id FROM SALLE")
+        return [i[0] for i in res.fetchall()]
 
-def recuperer_liste_couloir()->list:
-    # Renvoie une liste des id de tout les couloirs
-    res = cur.execute("SELECT id FROM SALLE")
-    return [i[0] for i in res.fetchall()]
+    def recuperer_theme_couloir(self,couloir_id: int)->str:
+        # Renvoie le thème du couloir à partir de son id
+        res = self.cur.execute("SELECT theme FROM SALLE WHERE ID=?",(couloir_id,))
+        return res.fetchone()[0]
 
-def recuperer_theme_couloir(couloir_id: int)->str:
-    # Renvoie le thème du couloir à partir de son id
-    res = cur.execute("SELECT theme FROM SALLE WHERE ID=?",(couloir_id,))
-    return res.fetchone()[0]
+    def recuperer_liste_tableau(self,couloir_id: int)->list:
+        # Renvoie une liste des id des tableaux appartenant a un couloir
+        res = self.cur.execute("SELECT id FROM TABLEAUX WHERE salle_id=?",(couloir_id,))
+        return [i[0] for i in res.fetchall()]
 
-def recuperer_liste_tableau(couloir_id: int)->list:
-    # Renvoie une liste des id des tableaux appartenant a un couloir
-    res = cur.execute("SELECT id FROM TABLEAUX WHERE salle_id=?",(couloir_id,))
-    return [i[0] for i in res.fetchall()]
+    def recuperer_nom_tableau(self,tableau_id: int)->str:
+        # Renvoie le nom du tableau à partir de son id
+        res = self.cur.execute("SELECT titre FROM TABLEAUX WHERE id=?",(tableau_id,))
+        return res.fetchone()[0]
 
-def recuperer_nom_tableau(tableau_id: int)->str:
-    # Renvoie le nom du tableau à partir de son id
-    res = cur.execute("SELECT titre FROM TABLEAUX WHERE id=?",(tableau_id,))
-    return res.fetchone()[0]
+    def recuperer_auteur_tableau(self,tableau_id: int)->str:
+        # Renvoie l'auteur du tableau à partir de son id
+        res = self.cur.execute("SELECT nom FROM TABLEAUX JOIN AUTEUR ON TABLEAUX.id=AUTEUR.id WHERE TABLEAUX.id=?",(tableau_id,))
+        return res.fetchone()[0]
 
-def recuperer_auteur_tableau(tableau_id: int)->str:
-    # Renvoie l'auteur du tableau à partir de son id
-    res = cur.execute("SELECT nom FROM TABLEAUX JOIN AUTEUR ON TABLEAUX.id=AUTEUR.id WHERE TABLEAUX.id=?",(tableau_id,))
-    return res.fetchone()[0]
+    def recuperer_description_tableau(self,tableau_id: int)->str:
+        # Renvoie la description du tableau à partir de son id
+        # A faire
+        pass
 
-def recuperer_description_tableau(tableau_id: int)->str:
-    # Renvoie la description du tableau à partir de son id
-    # A faire
-    pass
-
-def recuperer_date_tableau(tableau_id: int)->str:
-    # Renvoie la date du tableau à partir de son id
-    # A faire
-    pass
-
-print(recuperer_liste_tableau(2))
-print(recuperer_auteur_tableau(1))
-print(recuperer_auteur_tableau(2))
+    def recuperer_date_tableau(self,tableau_id: int)->str:
+        # Renvoie la date du tableau à partir de son id
+        # A faire
+        pass
